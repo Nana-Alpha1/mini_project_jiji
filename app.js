fbe363cac96ba3c174fe5fa095eac867f232f118
don <don@gmail.com>
2024-06-20 22:26:34 +0000

    Done

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const MongoDB = require("./utils/connectMongoDB");
 
 
 const app = express()
+const cors = require('cors');
+app.use(cors());
 app.set('view engine', 'ejs');
 app.use(bodyParser());
 app.use(bodyParserTwo.urlencoded({extended: true}));
